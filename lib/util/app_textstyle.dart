e264b480cb435d57d51bc4f98043e16c36ee16bb
Yun Hyerim <yunhyerim41@gmail.com>
2023-10-04 12:13:01 +0900

    commit

---
+++ lib/util/app_textstyle.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:your_type_mbti/util/app_color.dart';
+
+class AppTextstyle {
+  static TextStyle koPtBold36({Color color = AppColor.black}) =>
+      TextStyle(fontFamily: 'PretendardBold', fontSize: 36, color: color);
+
+  static TextStyle koPtBold30({Color color = AppColor.black}) =>
+      TextStyle(fontFamily: 'PretendardBold', fontSize: 30, color: color);
+
+  static TextStyle koPtBold25({Color color = AppColor.black}) =>
+      TextStyle(fontFamily: 'PretendardBold', fontSize: 25, color: color);
+
+  static TextStyle koPtBold20({Color color = AppColor.black}) =>
+      TextStyle(fontFamily: 'PretendardBold', fontSize: 20, color: color);
+
+  static TextStyle koPtBold15({Color color = AppColor.black}) =>
+      TextStyle(fontFamily: 'PretendardBold', fontSize: 15, color: color);
+
+  static TextStyle koPtBold10({Color color = AppColor.black}) =>
+      TextStyle(fontFamily: 'PretendardBold', fontSize: 10, color: color);
+
+  static TextStyle koPtSemiBold36({Color color = AppColor.black}) =>
+      TextStyle(fontFamily: 'PretendardSemiBold', fontSize: 36, color: color);
+
+  static TextStyle koPtSemiBold30({Color color = AppColor.black}) =>
+      TextStyle(fontFamily: 'PretendardSemiBold', fontSize: 30, color: color);
+
+  static TextStyle koPtSemiBold25({Color color = AppColor.black}) =>
+      TextStyle(fontFamily: 'PretendardSemiBold', fontSize: 25, color: color);
+
+  static TextStyle koPtSemiBold20({Color color = AppColor.black}) =>
+      TextStyle(fontFamily: 'PretendardSemiBold', fontSize: 20, color: color);
+
+  static TextStyle koPtSemiBold15({Color color = AppColor.black}) =>
+      TextStyle(fontFamily: 'PretendardSemiBold', fontSize: 15, color: color);
+
+  static TextStyle koPtSemiBold10({Color color = AppColor.black}) =>
+      TextStyle(fontFamily: 'PretendardSemiBold', fontSize: 10, color: color);
+
+  static TextStyle koPtRegular36({Color color = AppColor.black}) =>
+      TextStyle(fontFamily: 'PretendardRegular', fontSize: 36, color: color);
+
+  static TextStyle koPtRegular30({Color color = AppColor.black}) =>
+      TextStyle(fontFamily: 'PretendardRegular', fontSize: 30, color: color);
+
+  static TextStyle koPtRegular25({Color color = AppColor.black}) =>
+      TextStyle(fontFamily: 'PretendardRegular', fontSize: 25, color: color);
+
+  static TextStyle koPtRegular20({Color color = AppColor.black}) =>
+      TextStyle(fontFamily: 'PretendardRegular', fontSize: 20, color: color);
+
+  static TextStyle koPtRegular15({Color color = AppColor.black}) =>
+      TextStyle(fontFamily: 'PretendardRegular', fontSize: 15, color: color);
+
+  static TextStyle koPtRegular10({Color color = AppColor.black}) =>
+      TextStyle(fontFamily: 'PretendardRegular', fontSize: 10, color: color);
+}
